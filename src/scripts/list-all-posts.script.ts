0d6941c91aaf571f0e2d3fb456482187997e4fec
Dante Calderon <mail@dantecalderon.dev>
2020-06-29 02:28:19 -0500

    Revert "Add scripts to create"
    This reverts commit 53d72b95f0d2b80fc4327b8445d25862c8696ed8.

--- src/scripts/list-all-posts.script.ts
+++
@@ -1,9 +0,0 @@
-import { executeScript } from '../mongoose.connection'
-import { Post } from '../post.schema'
-
-executeScript(main)
-
-async function main() {
-  const posts = await Post.find({})
-  console.log('Dante: main -> posts', posts)
-}
